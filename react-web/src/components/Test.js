56dc998530e73963aed42f31c0e0bc440498306b
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2022-02-27 20:30:18 +0100

    unknown

---
+++ react-web/src/components/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test() {
+    return (
+        <div>
+            <h1>Hello World! x2</h1>
+        </div>
+    );
+}
+
+export default Test;
--- react-web/src/index.js
+++ react-web/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-
+import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
