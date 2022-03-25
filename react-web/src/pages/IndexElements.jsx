d279d201b190d3982ecaa65e1eae7661ad192581
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2022-03-26 00:58:56 +0100

    1.0.4 react-web

---
+++ react-web/src/pages/IndexElements.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+
+
+export const SMain = styled.div`
+    position: absolute;
+    margin: 0;
+    height: 80%;
+    width: 100%;
+    background-color: black;
+`;
+export const Background = styled.div`
+    background-image: url("img_tree.gif"), url("paper.gif");
+    background-repeat: no-repeat, repeat;
+`;
+export const H1Title = styled.h1`
+  color: red;
+`;
