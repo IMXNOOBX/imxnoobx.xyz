1e513e85ff933b2f7c0165f2f72179e2c2a2b44c
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2022-07-10 17:57:21 +0200

    web - 0.5.0

---
+++ web/src/elements/components/nav_bar/index.jsx
@@ -0,0 +1,49 @@
+// import React, { useState } from "react";
+import React from "react";
+import { IconContext } from 'react-icons'
+import { IoLogoGithub, IoLogoSteam, IoHomeOutline, IoLogoDiscord } from 'react-icons/io5'
+// import { FaDiscord } from 'react-icons/fa'
+import {
+	// SMain,
+	// ContainerFluid,
+	// NoSelect,
+	// CoolEffect,
+	NavBar,
+	NavBarUl,
+	NavBarLi,
+	NavBara,
+	// IoIcon,
+	NavBarBTN,
+} from './IndexElements';
+
+function Main({useRef}) {
+  
+	return (
+	  <div>
+		<NavBar>
+			<NavBarUl>
+				<IconContext.Provider value={{ color: "#fff" }}>
+					<NavBarLi><NavBarBTN onClick={() => useRef.current.scrollTo(1)}>{/* <NavBara href="https://github.com/IMXNOOBX" target="noreferrer"> */}
+						<IoLogoGithub/>
+					</NavBarBTN></NavBarLi> {/* </NavBara></NavBarLi>  */}
+					<NavBarLi><NavBara href="https://steamcommunity.com/id/IMXNOOBX/" target="noreferrer">
+						<IoLogoSteam/>
+					</NavBara></NavBarLi> 
+					<NavBarLi><NavBarBTN onClick={() => useRef.current.scrollTo(0)}>
+						<IoHomeOutline/>
+					</NavBarBTN></NavBarLi> 
+					<NavBarLi><NavBara href="https://steamcommunity.com/id/ITSXNOOBX/" target="noreferrer">
+						<IoLogoSteam/>
+					</NavBara></NavBarLi>
+					<NavBarLi><NavBara href="https://discord.gg/m68Nfgyb4m" target="noreferrer">
+						<IoLogoDiscord/>
+					</NavBara></NavBarLi>
+				</IconContext.Provider>
+			</NavBarUl>
+		</NavBar>
+	  </div>
+	);
+  }
+  
+  // https://react-icons.github.io/react-icons/icons?name=io5
+  export default Main;
